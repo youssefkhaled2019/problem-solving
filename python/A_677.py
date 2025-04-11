ee99939516e88f7b8ebb9ca9a8b359cd5bbe1b16
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-04-11 19:53:03 +0200

    day1

---
+++ python/A_677.py
@@ -0,0 +1,16 @@
+"""
+contest = https://codeforces.com/contest/677/problem/A
+date = Tuesday, May 9, 2023
+
+"""
+
+n,h=list(map(int,input().split(" ")))
+l=list(map(int,input().split(" ")))
+s=0
+for i in range(n):
+    if (l[i]>h):
+        s+=2
+    else:
+        s+=1
+ 
+print(s)
---
+++ python/A_734.py
@@ -0,0 +1,16 @@
+"""
+contest = https://codeforces.com/contest/734/problem/A
+date = Tuesday, May 9, 2023
+
+"""
+
+n=int(input())
+s=input()
+a=s.count("A")
+d=n-a
+if(a>d):
+    print("Anton")
+elif(a<d):
+     print("Danik")
+else:   
+     print("Friendship")
